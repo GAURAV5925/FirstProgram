9e6efa404172f7c19588924e2f53486797d3c2f1
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2022-08-29 19:22:04 +0530

    Update Student.java

--- OOP/ConstructorOverl/Student.java
+++ OOP/ConstructorOverl/Student.java
@@ -14,7 +14,7 @@ public class Student {
     public static void main(String[] args) {
         Student s = new Student();
         System.out.println("\nDefault CONSTRUCTOR values:\n");
-        System.out.println("Student ID: " + s.id + "\nStudent Name:"+s.name);
+        System.out.println("Student ID: " + s.id + "\nStudent's Name:"+s.name);
         System.out.println("\nParameterized Constructor values:\n");
         Student student = new Student(10, "parth");
         System.out.println("Student Id" + student.id + "\nStudent Name: " + student.name);
